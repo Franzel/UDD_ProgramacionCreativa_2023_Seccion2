c6aed27d51e92b64b903d1288451f92c91ab6899
Francisco Zamorano <Franzel@users.noreply.github.com>
2023-04-11 15:30:46 -0400

    corrigindo S5 S6 S7

--- p5/S7/rotadores/sketch.js
+++ p5/S7/rotadores/sketch.js
@@ -1,26 +1,28 @@
-let nCols = 50;
-let nFilas = 50;
+let nCols = 60;
+let nFilas = 60;
 let angulos = [];
 let velocidades = [];
+let selector;
 
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(800, 800);
+  rectMode(CENTER);
+  selector = 0;
+
   for (let i = 0; i < nCols; i++) {
     angulos[i] = [];
     velocidades[i] = [];
     
     for (let j = 0; j < nFilas; j++) {
-      angulos[i][j] = random(0, 360);
-      velocidades[i][j] = random(-3,3);
+      angulos[i][j] = 90 + random(-10, 10);
+      velocidades[i][j] = random(-0.5,0.5);
     }
   }
-  rectMode(CENTER);
+  
 }
 
 function draw() {
   background(0);
-
-
   for (let i = 0; i < angulos.length; i++) {
     for (let j = 0; j < angulos[1].length; j++) {
       angulos[i][j] += velocidades[i][j];
@@ -33,6 +35,30 @@ function rotador( x,  y,  a,  s) {
   push();
   translate(x, y);
   rotate(radians(a));
-  rect(0, 0, s, s);
+
+  if(selector==1){
+    stroke(255);
+    line(0, 0, 0, -(s/3)*2);
+  } else if (selector==2) {
+    stroke(255);
+    line(0, (s/3)*2, 0, -(s/3)*2);
+    line((s/3)*2, 0, -(s/3)*2, 0);
+  } else{
+    rect(0, 0, s, s);  
+  } 
   pop();
+}
+
+function keyPressed(){
+  if (key=='a') {
+    selector=0;
+  } else if (key=='s') {
+    selector=1;
+  } else if (key=='d') {
+    selector=2;  
+  } else{
+    selector=0;
+  }
+
+  
 }
